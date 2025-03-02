304d0942f26907da30531f997c4ee502389c9312
CanhVeTaiGia <lvy16012005@gmail.com>
2025-03-03 00:26:36 +0700

    add

---
+++ ss2/src/b1.java
@@ -0,0 +1,12 @@
+public class b1 {
+    public static void main(String[] args) {
+        int a = 9;
+        double db = 5.9;
+        char ch = 'c';
+        String str = "abc";
+        System.out.println("Giá trị biến a = " + a);
+        System.out.println("Giá trị biến db = " + db);
+        System.out.println("Giá trị biến ch = " + ch);
+        System.out.println("Giá trị biến String = " + str);
+    }
+}
