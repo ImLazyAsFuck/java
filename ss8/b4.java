cbf5d7ef736071b4600be205a12b2badcc3451c0
CanhVeTaiGia <lvy16012005@gmail.com>
2025-03-10 09:14:10 +0700

    add

--- ss8/b4.java
+++ ss8/b4.java
@@ -1,45 +1,49 @@
 public class b4 {
     public static void main(String[] args) {
-        String initialString = "abcdefghij".repeat(100000);
+        StringBuilder sb = new StringBuilder("abcdefghij".repeat(100000));
+        StringBuffer sbf = new StringBuffer("abcdefghij".repeat(100000));
 
-        System.out.println("Testing StringBuilder...");
-        testPerformance(new StringBuilder(initialString));
-
-        System.out.println("\nTesting StringBuffer...");
-        testPerformance(new StringBuffer(initialString));
-    }
-
-    private static void testPerformance(Object obj) {
         long startTime, endTime;
 
-        if (!(obj instanceof StringBuilder) && !(obj instanceof StringBuffer)) {
-            return;
-        }
-
-        StringBuilder sb = obj instanceof StringBuilder ? (StringBuilder) obj : null;
-        StringBuffer sbf = obj instanceof StringBuffer ? (StringBuffer) obj : null;
-
+        System.out.println("Testing StringBuilder...");
         startTime = System.nanoTime();
-        if (sb != null) sb.reverse();
-        else if (sbf != null) sbf.reverse();
+        sb.reverse();
         endTime = System.nanoTime();
         System.out.println("Reverse time: " + (endTime - startTime) + " ns");
 
         startTime = System.nanoTime();
-        if (sb != null) sb.insert(500000, "INSERTED");
-        else if (sbf != null) sbf.insert(500000, "INSERTED");
+        sb.insert(500000, "INSERTED");
         endTime = System.nanoTime();
         System.out.println("Insert time: " + (endTime - startTime) + " ns");
 
         startTime = System.nanoTime();
-        if (sb != null) sb.delete(400000, 400010);
-        else if (sbf != null) sbf.delete(400000, 400010);
+        sb.delete(400000, 400010);
         endTime = System.nanoTime();
         System.out.println("Delete time: " + (endTime - startTime) + " ns");
 
         startTime = System.nanoTime();
-        if (sb != null) sb.replace(300000, 300010, "REPLACED");
-        else if (sbf != null) sbf.replace(300000, 300010, "REPLACED");
+        sb.replace(300000, 300010, "REPLACED");
+        endTime = System.nanoTime();
+        System.out.println("Replace time: " + (endTime - startTime) + " ns");
+
+        System.out.println("\nTesting StringBuffer...");
+        startTime = System.nanoTime();
+        sbf.reverse();
+        endTime = System.nanoTime();
+        System.out.println("Reverse time: " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        sbf.insert(500000, "INSERTED");
+        endTime = System.nanoTime();
+        System.out.println("Insert time: " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        sbf.delete(400000, 400010);
+        endTime = System.nanoTime();
+        System.out.println("Delete time: " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        sbf.replace(300000, 300010, "REPLACED");
         endTime = System.nanoTime();
         System.out.println("Replace time: " + (endTime - startTime) + " ns");
     }
