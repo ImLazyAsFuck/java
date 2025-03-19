d0e1393083adbc0ec5af3bd8b5a1bfbdd9980ea6
CanhVeTaiGia <lvy16012005@gmail.com>
2025-03-19 17:28:27 +0700

    add

--- PTIT-K23B-LuongHaMinhVy-01/src/ra/bussiness/StudentBussiness.java
+++ PTIT-K23B-LuongHaMinhVy-01/src/ra/bussiness/StudentBussiness.java
@@ -1,7 +1,45 @@
 package ra.bussiness;
 
+import ra.entity.Student;
+
+import java.util.Scanner;
+
 public class StudentBussiness{
-    public void menu(){
-        System.out.println("");
+    public static void menu(){
+        System.out.println("---------------------------Student Menu----------------------");
+        System.out.println("1. Hiển thị danh sách sinh viên");
+        System.out.println("2. Thêm mới sinh viên");
+        System.out.println("3. Chỉnh sửa thông tin sinh viên");
+        System.out.println("4. Xoá sinh viên");
+        System.out.println("5. Tìm kiếm sinh viên");
+        System.out.println("6. Sắp xếp");
+        System.out.println("0. Thoát chương trình");
     }
+
+    public static void displayStudents(Student[] students, int currentIndex){
+        if(currentIndex == 0){
+            System.out.println("Mảng rỗng");
+        }
+        for(int i = 0; i < currentIndex; i++){
+            students[i].displayData();
+        }
+    }
+
+   public static void addStudent(Scanner input, Student[] students, int currentIndex){
+       System.out.print("Nhập số lượng sinh viên: ");
+       int amounts = Integer.parseInt(input.nextLine());
+       if(currentIndex + amounts >= students.length){
+           System.out.println("Vượt quá giới hạn danh sách sinh viên.");
+           return;
+       }
+       for(int i = currentIndex; i < amounts; i++){
+           students[i].inputData(input);
+       }
+   }
+
+   public static void updateStudent(Scanner input, Student[] students, int currentIndex){
+        for(int i = 0; i < currentIndex; i++){
+
+        }
+   }
 }
--- PTIT-K23B-LuongHaMinhVy-01/src/ra/presentation/StudentApplication.java
+++ PTIT-K23B-LuongHaMinhVy-01/src/ra/presentation/StudentApplication.java
@@ -1,5 +1,6 @@
 package ra.presentation;
 
+import ra.bussiness.StudentBussiness;
 import ra.entity.Student;
 
 import java.util.Scanner;
@@ -9,5 +10,30 @@ public class StudentApplication{
         Scanner input  = new Scanner(System.in);
         final Student[] students = new Student[100];
         int currentIndex = 0;
+        while(true){
+            StudentBussiness.menu();
+            System.out.print("Mời bạn chọn: ");
+            int choice = Integer.parseInt(input.nextLine());
+            switch(choice){
+                case 1:
+                    StudentBussiness.displayStudents(students, currentIndex);
+                    break;
+                case 2:
+                    StudentBussiness.addStudent(input, students, currentIndex);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.err.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+            }
+        }
     }
 }
